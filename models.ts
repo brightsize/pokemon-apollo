3586448f11db557205ba818b8ed54a03db19ae9b
Tristan Guest <tristanguest@gmail.com>
2022-05-30 20:42:51 -0300

    Add pokemon type interface

--- models.ts
+++ models.ts
@@ -2,3 +2,19 @@ export type DocumentHead = {
   title: string;
   description: string;
 };
+
+export interface Pokemon {
+  classification: string;
+  fleeRate: string;
+  height: { minimum: string; maximum: string };
+  id: string;
+  image: string;
+  maxCP: number;
+  maxHP: number;
+  name: string;
+  number: string;
+  resistant: string[];
+  types: string[];
+  weaknesses: string[];
+  weight: { minimum: string; maximum: string };
+}
