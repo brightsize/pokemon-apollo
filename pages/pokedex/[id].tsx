5cd190d9cb636ecaddbdcc13dd17aa4985801c79
Tristan Guest <tristanguest@gmail.com>
2022-05-30 19:46:13 -0300

    Fetch pokemon data based on [id] route

--- pages/pokedex/[id].tsx
+++ pages/pokedex/[id].tsx
@@ -1,15 +1,61 @@
-import type { InferGetServerSidePropsType, NextPage } from "next";
+import { gql } from "@apollo/client";
+import type {
+  GetServerSideProps,
+  InferGetServerSidePropsType,
+  NextPage,
+} from "next";
+import { getApolloClient } from "../../apollo-client";
 import styles from "../../styles/Page.module.css";
 
-export const getServerSideProps = async () => {
+import { Card } from "../../components";
+import Image from "next/image";
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { id } = context.query;
+
+  const client = getApolloClient();
+
+  const { data } = await client.query({
+    query: gql`
+      query pokemon($id: String) {
+        pokemon(id: $id) {
+          id
+          number
+          name
+          weight {
+            minimum
+            maximum
+          }
+          height {
+            minimum
+            maximum
+          }
+          classification
+          types
+          resistant
+          weaknesses
+          fleeRate
+          maxCP
+          maxHP
+          image
+        }
+      }
+    `,
+    variables: {
+      id: id,
+    },
+  });
+
   return {
-    props: {},
+    props: {
+      pokemon: data.pokemon,
+    },
   };
 };
 
 const Pokemon: NextPage<
   InferGetServerSidePropsType<typeof getServerSideProps>
-> = () => {
+> = ({ pokemon }) => {
   return (
     <main className={styles.main}>
       <h1 className={styles.title}>Think of a clever title</h1>
