969eda2b5a4e14824b026efdb326ed90b22ea3b7
jordayn <75748708+jordayn@users.noreply.github.com>
2022-06-07 22:50:48 -0300

    started iss4, some more questions tomorrow

--- pages/pokedex/index.tsx
+++ pages/pokedex/index.tsx
@@ -1,24 +1,51 @@
-import type { InferGetServerSidePropsType, NextPage } from "next";
-import { CardGrid } from "../../components";
-import styles from "../../styles/Page.module.css";
+import { gql } from '@apollo/client';
+import type { Pokemon } from '..';
+import { getApolloClient } from '../../apollo-client';
+import type { InferGetServerSidePropsType, NextPage } from 'next';
+import { CardGrid } from '../../components';
+import styles from '../../styles/Page.module.css';
+import { PokemonCard } from '../../components';
+import { POKEMON } from '../../config';
+import type { PokemonCardProps } from '../../components';
+import { getRandomPokemonId } from '../../utils';
 
 export const getServerSideProps = async () => {
+  const client = getApolloClient();
+
+  const { data } = await client.query({
+    query: gql`
+      query {
+        pokemons(first: 20) {
+          id
+          name
+          number
+          image
+        }
+      }
+    `,
+  });
   return {
-    props: {},
+    props: {
+      data: data.pokemons,
+    },
   };
 };
 
 const Pokedex: NextPage<
   InferGetServerSidePropsType<typeof getServerSideProps>
-> = () => {
+> = ({ data }) => {
   return (
     <main className={styles.main}>
-      <h1 className={styles.title}>Think of a clever title</h1>
+      <h1 className={styles.title}>The PokemonCard calls them all featured</h1>
 
-      <p className={styles.description}>Think of a clever description</p>
+      <p className={styles.description}>
+        All the {POKEMON} are <i>featured</i> here!
+      </p>
 
       <CardGrid>
-        Map the pokemon here into {"<PokemonCard />"} components
+        {data.map((pokemon: Pokemon) => (
+          <PokemonCard key={pokemon.id} {...pokemon} />
+        ))}
       </CardGrid>
     </main>
   );
