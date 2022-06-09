e07e9c393d855cc5ea6cd071792bd654a1bb6077
Tristan Guest <tristanguest@gmail.com>
2022-06-09 20:31:42 -0300

    Build featured pokemon card with queried data

---
+++ components/organisms/FeatureCard/FeatureCard.tsx
@@ -0,0 +1,96 @@
+import Image from "next/image";
+import React from "react";
+import styles from "./FeatureCard.module.css";
+import { Card } from "../../../components";
+import { Pokemon } from "../../../models";
+
+const FeatureCard = ({
+  classification,
+  fleeRate,
+  height,
+  image,
+  maxCP,
+  maxHP,
+  name,
+  number,
+  resistant,
+  types,
+  weaknesses,
+  weight,
+}: Pokemon) => {
+  const typeBadges = types.map((type) => {
+    return (
+      <div key={type} className={styles.type_badge}>
+        {type}
+      </div>
+    );
+  });
+
+  const weaknessBadges = weaknesses.map((type) => {
+    return (
+      <div key={type} className={styles.type_badge}>
+        {type}
+      </div>
+    );
+  });
+
+  const resistanceBadges = resistant.map((type) => {
+    return (
+      <div key={type} className={styles.type_badge}>
+        {type}
+      </div>
+    );
+  });
+
+  return (
+    <Card>
+      <div className={styles.container}>
+        <div className={styles.titles}>
+          <h1>
+            {name} #{number}
+          </h1>
+          <h2>{classification}</h2>
+          {types.length > 1 ? <strong>Types</strong> : <strong>Type</strong>}
+          <span className={styles.type_list}>{typeBadges}</span>
+        </div>
+        <div className={styles.image}>
+          <Image src={image} layout="fill" objectFit="contain" alt={name} />
+        </div>
+        <div className={styles.content}>
+          <h3>Resistances</h3>
+          <span className={styles.type_list}>{resistanceBadges}</span>
+          <h3>Weaknesses</h3>
+          <span className={styles.type_list}>{weaknessBadges}</span>
+          <div className={styles.stat_block}>
+            <div>
+              <h3>Max HP</h3>
+              <p>{maxHP}</p>
+            </div>
+            <div>
+              <h3>Max CP</h3>
+              <p>{maxCP}</p>
+            </div>
+            <div>
+              <h3>Height</h3>
+              <p>
+                {height.minimum} - {height.maximum}
+              </p>
+            </div>
+            <div>
+              <h3>Weight</h3>
+              <p>
+                {weight.minimum} - {weight.maximum}
+              </p>
+            </div>
+            <div>
+              <h3>Flee rate</h3>
+              <p>{fleeRate}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Card>
+  );
+};
+
+export { FeatureCard };
--- pages/pokedex/[id].tsx
+++ pages/pokedex/[id].tsx
@@ -6,9 +6,7 @@ import type {
 } from "next";
 import { getApolloClient } from "../../apollo-client";
 import styles from "../../styles/Page.module.css";
-
-import { Card } from "../../components";
-import Image from "next/image";
+import { FeatureCard } from "../../components/organisms/FeatureCard";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const { id } = context.query;
@@ -58,9 +56,7 @@ const Pokemon: NextPage<
 > = ({ pokemon }) => {
   return (
     <main className={styles.main}>
-      <h1 className={styles.title}>Think of a clever title</h1>
-      <p className={styles.description}>Think of a clever description</p>
-      Think of a good way to represent all the pokemon details
+      <FeatureCard {...pokemon} />
     </main>
   );
 };
