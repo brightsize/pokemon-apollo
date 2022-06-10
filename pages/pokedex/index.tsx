d7c3a53359530282b7ca80e8e1f35ffb6ef67438
jordayn <75748708+jordayn@users.noreply.github.com>
2022-06-10 11:12:13 -0300

    typing the response

--- pages/pokedex/index.tsx
+++ pages/pokedex/index.tsx
@@ -1,16 +1,20 @@
 import { gql } from '@apollo/client';
-import type { Pokemon } from '..';
 import { getApolloClient } from '../../apollo-client';
 import type { InferGetServerSidePropsType, NextPage } from 'next';
+import type { Pokemon } from '..';
 import { CardGrid } from '../../components';
 import styles from '../../styles/Page.module.css';
 import { PokemonCard } from '../../components';
 import { POKEMON } from '../../config';
 
+type Pokemons = {
+  pokemons: [Pokemon];
+};
+
 export const getServerSideProps = async () => {
   const client = getApolloClient();
 
-  const { data } = await client.query({
+  const { data } = await client.query<Pokemons>({
     query: gql`
       query {
         pokemons(first: 20) {
@@ -24,15 +28,14 @@ export const getServerSideProps = async () => {
   });
   return {
     props: {
-      data: data.pokemons,
+      pokes: data.pokemons,
     },
   };
 };
 
 const Pokedex: NextPage<
   InferGetServerSidePropsType<typeof getServerSideProps>
-> = ({ data }) => {
-  const pokes = data;
+> = ({ pokes }) => {
   return (
     <main className={styles.main}>
       <h1 className={styles.title}>Think of a clever title</h1>
